2a41d692782f8c3a3c2a6de5b85c78333503aec5
Horashi <horashidev@gmail.com>
2025-04-12 08:26:27 +0700

    video 74

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,9 +1,10 @@
 import Input from "antd/es/input/Input"
 import Password from "antd/es/input/Password"
 import "./user.css"
-import { Button, Form } from "antd"
+import { Button, Descriptions, Form, message, notification, Modal } from "antd"
 import { useState } from "react"
 import axios from "axios"
+import { createUserAPI } from "../../services/axios.service"
 
 
 const UserForm = () => {
@@ -11,43 +12,70 @@ const UserForm = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [phone, setPhone] = useState("")
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    // const handleOk = () => {
+    //     setIsModalOpen(false);
+    // };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName,
-            email,
-            password,
-            phone
+    const handleSubmitBtn = async () => {
+        // alert("Clicked")
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success(
+                {
+                    message: "Create user successful",
+                    description: "Tạo user thành công",
+                }
+            )
         }
-        axios.post(URL_BACKEND, data)
-        console.log(fullName, email, password, phone)
-        console.log(axios.interceptors.response);
-        
+        else {
+            notification.error(
+                {
+                    message: "Create user failed",
+                    description: JSON.stringify(res.data.message.join(" ,"))
+                }
+            )
+        }
     }
-    
+
     return (
         <div className="user-form">
-            <Form>
-                <div>
-                    <label>Full name:</label>
-                    <Input placeholder="Full name" onChange={()=>setFullName(event.target.value)}/>
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <Input type="email" placeholder="example@gmail.com" onChange={()=>setEmail(event.target.value)}/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <Password onChange={()=>setPassword(event.target.value)}/>
-                </div>
-                <div>
-                    <label>Phone:</label>
-                    <Input type="number" onChange={()=>setPhone(event.target.value)}/>
-                </div>
-                <Button type="primary" onClick={()=>handleClickBtn()}>Create</Button>
-            </Form>
-
+            <div style={{ display: "flex", justifyContent: "space-between"}}>
+                <h3>Table Users</h3>
+                <Button type="primary" onClick={() => showModal()}>Create</Button>
+            </div>
+            <Modal
+                title="Create User"
+                open={isModalOpen}
+                onOk={handleSubmitBtn}
+                onCancel={handleCancel}
+                okText={"CREATE"}
+            >
+                <Form style={{display:"flex", gap: "10px", flexDirection: "column"}}>
+                    <div>
+                        <label>Full Name:</label>
+                        <Input placeholder="Full name" onChange={() => setFullName(event.target.value)} />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <Input type="email" placeholder="example@gmail.com" onChange={() => setEmail(event.target.value)} />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <Password onChange={() => setPassword(event.target.value)} />
+                    </div>
+                    <div>
+                        <label>Phone number</label>
+                        <Input type="number" onChange={() => setPhone(event.target.value)} />
+                    </div>
+                </Form>
+            </Modal>
         </div>
     )
 }
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,8 +1,64 @@
+import { Space, Table, Tag } from 'antd';
+import { useEffect, useState } from 'react';
+import { fetchAllUserAPI } from '../../services/axios.service';
+
+
 const UserTable = () => {
+    const [dataUsers, setDataUsers] = useState([])
+    
+    //empty array
+    useEffect(()=>{
+        console.log("Run useEffect 111")
+        loadUser()
+    },[])
+
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: '_id',
+        },
+        {
+            title: 'Full Name',
+            dataIndex: 'fullName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+    ];
+    // const data = [
+    //     {
+    //         key: '1',
+    //         name: 'John Brown',
+    //         age: 32,
+    //         address: 'New York No. 1 Lake Park',
+    //         tags: ['nice', 'developer'],
+    //     },
+    //     {
+    //         key: '2',
+    //         name: 'Jim Green',
+    //         age: 42,
+    //         address: 'London No. 1 Lake Park',
+    //         tags: ['loser'],
+    //     },
+    //     {
+    //         key: '3',
+    //         name: 'Joe Black',
+    //         age: 32,
+    //         address: 'Sydney No. 1 Lake Park',
+    //         tags: ['cool', 'teacher'],
+    //     },
+    // ];
+
+
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI()
+        setDataUsers(res.data)
+    }
+
     return (
-        <div className="user-table">
-        
-        </div>
+        <Table columns={columns} dataSource={dataUsers} rowKey={"_id"}/>
     )
 }
+
 export default UserTable
